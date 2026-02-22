9604558b6bb42801cea9832de83aad3d595c9179
Divyang Dheer <divyangdheer11@gmail.com>
2026-02-22 23:44:30 +0530

    fix: wrapped update user controller in try catch

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,38 +18,43 @@ const getUsers = async (req, res) => {
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = async (req, res) => {
-  const user = await User.findById(req.user._id);
+  try {
+    const user = await User.findById(req.user._id);
 
-  if (user) {
-    user.name = req.body.name || user.name;
-    user.dob = req.body.dob || user.dob;
-    user.joiningDate = req.body.joiningDate || user.joiningDate;
-    if (req.body.isOnLeave !== undefined) {
-      user.isOnLeave = req.body.isOnLeave;
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.dob = req.body.dob || user.dob;
+      user.joiningDate = req.body.joiningDate || user.joiningDate;
+      if (req.body.isOnLeave !== undefined) {
+        user.isOnLeave = req.body.isOnLeave;
+      }
+      if (req.file) {
+        user.profilePhoto = `/uploads/${req.file.filename}`;
+      } else if (req.body.profilePhoto) {
+        user.profilePhoto = req.body.profilePhoto;
+      }
+
+      const updatedUser = await user.save();
+
+      res.json({
+        _id: updatedUser._id,
+        username: updatedUser.username,
+        name: updatedUser.name,
+        dob: updatedUser.dob,
+        joiningDate: updatedUser.joiningDate,
+        isOnLeave: updatedUser.isOnLeave,
+        profilePhoto: updatedUser.profilePhoto,
+        organization: updatedUser.organization,
+        role: updatedUser.role,
+        status: updatedUser.status,
+      });
+    } else {
+      res.status(404);
+      throw new Error("User not found");
     }
-    if (req.file) {
-      user.profilePhoto = `/uploads/${req.file.filename}`;
-    } else if (req.body.profilePhoto) {
-      user.profilePhoto = req.body.profilePhoto;
-    }
-
-    const updatedUser = await user.save();
-
-    res.json({
-      _id: updatedUser._id,
-      username: updatedUser.username,
-      name: updatedUser.name,
-      dob: updatedUser.dob,
-      joiningDate: updatedUser.joiningDate,
-      isOnLeave: updatedUser.isOnLeave,
-      profilePhoto: updatedUser.profilePhoto,
-      organization: updatedUser.organization,
-      role: updatedUser.role,
-      status: updatedUser.status,
-    });
-  } else {
-    res.status(404);
-    throw new Error("User not found");
+  } catch (error) {
+    console.log("🚀 -> updateUserProfile -> error:", error);
+    res.status(500).json({ message: error.message });
   }
 };
 
